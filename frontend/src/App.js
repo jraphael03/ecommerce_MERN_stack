b1b924471b1c17f8ad6992efdf68047de42e0b68
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-10 23:41:53 -0500

    Created a HomeScreen.js, and ProductScreen.js, added code to index.css as well

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
 import react from "react";
-import Product from "./components/Product";
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a href="/" className="brand">
-            Ecommerce
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product.id} product={product} ></Product>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All Rights Reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a href="/" className="brand">
+              Ecommerce
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/' exact component={HomeScreen}></Route>
+
+        </main>
+        <footer className="row center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
