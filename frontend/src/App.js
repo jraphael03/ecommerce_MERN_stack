f621be2f9e73a00c5e7b4449f470107f367c58df
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-12 20:41:54 -0500

    Added react-redux to project, create react store, created productActions to create an action to get products from backend, productReducer created an update for redux store based on productList actions, in HomeScreen we removed the code to fetch data from the backend and dispatched a redux action to update redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
