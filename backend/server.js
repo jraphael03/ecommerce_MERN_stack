20d30732c2de53855c2e089e76e15b04ae6081c0
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-13 13:03:55 -0500

    added a new service api for productDetails, in storejs added productDetails reducer to the list of reducers in redux store, productAction we defined an action to return data about data, in product screen we used the action to fetch data and post to screen

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,19 @@ app.get('/api/products', (req, res) => {
     res.send(data.products)
 })
 
+
+// GRABBING PRODUCT FROM DATA.JS
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find( x => x._id === req.params.id);
+  if(product){
+      res.send(product);
+  } else{
+      res.status(404).send({ message: 'Product not Found' })
+  }
+});
+
+
+
 app.listen(port, () => {
     console.log(`Server is listening at ${port}`)
 })
