d8567fcdf9fc998bf91a1a75580ffc81647850ff
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-15 20:54:11 -0500

    Cart action defined removeFromCart, in cartReducer created filter function to remove item, cartScreen create removeFromCart to dispatch

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -13,8 +13,15 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           ), //compare product with existItem if they are equal to each other replace with item if they are not equal return previous value (x)
         };
       } else {
-        return { ...state, cartItems: [...state.cartItems, item] }      // concatenate cartitems with items (display everything in cart)
+        return { ...state, cartItems: [...state.cartItems, item] }; // concatenate cartitems with items (display everything in cart)
       }
+
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),   // Filter out id that is equal to action.payload  in cartAction payload = productId
+      };
+
     default:
       return state;
   }
